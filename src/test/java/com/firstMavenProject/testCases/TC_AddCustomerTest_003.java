a29db785a37db3aca9efe25d4d2f5d4493a0ca66
singhcapardeep <singh.ca.pardeep@gmail.com>
2020-09-08 15:50:32 -0700

    commit new

--- src/test/java/com/firstMavenProject/testCases/TC_AddCustomerTest_003.java
+++ src/test/java/com/firstMavenProject/testCases/TC_AddCustomerTest_003.java
@@ -19,7 +19,7 @@ public class TC_AddCustomerTest_003 extends BaseClass {
 		logger.info("Passsword is provided");
 		lp.clickButton();
 
-		Thread.sleep(3000);
+		Thread.sleep(4000);
 
 		AddCustomerPage addcust = new AddCustomerPage(driver);
 
